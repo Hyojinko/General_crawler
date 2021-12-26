d2df6b2abdb5936e7758a15b706b6159b3db710b
kohyojin <2rhgywls@gmail.com>
2021-12-26 20:32:37 +0900

    get_page_links

--- domain_general.py
+++ domain_general.py
@@ -20,21 +20,37 @@ import threading
 with open('crawled_urls.txt','r') as fd:
     urls = fd.read().splitlines()
 
-    def get_link(url):
+    '''def get_link(url):
         for u in url:
-            site = u
             # Takes the website you typed and stores it in > site < variable
 
             # urllib.request.urlopen(url).read()
-            make_request_to_site = urllib.request.urlopen(site).read()
+            make_request_to_site = urllib.request.urlopen(u).read()
             # Makes a request to the site that we stored in a var
             soup = BeautifulSoup(make_request_to_site, "html.parser")
             # We pass it through BeautifulSoup parser in this case html.parser
             # Next we make a loop to find all links in the site that we stored
             links = []
+            link_df = pd.DataFrame()
             for link in soup.findAll('a'):
-                links.append(link)
-            print(links)
-            return links
+                links.append(link.get('href'))
+                links = pd.Series(links, name=u)
+            #link_df[urlparse(u).netloc] = links
+        return links'''
+    def get_link(url):
+        make_request_to_site = urllib.request.urlopen(url).read()
+        soup = BeautifulSoup(make_request_to_site, "html.parser")
+        links = []
+        for link in soup.findAll('a'):
+            links.append(link.get('href'))
+        links = pd.Series(links, name=url)
+        return urlparse(url).netloc, links
 
-get_link(urls)
+    df_link = pd.DataFrame()
+    for u in urls:
+        domain, links = get_link(u)
+        df_link[domain] = links
+    print(df_link)
+
+
+#print(get_link(urls))
