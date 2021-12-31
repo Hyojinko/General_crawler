2fcc298939815527ed00d0e0b5de647bbb274dd7
kohyojin <2rhgywls@gmail.com>
2021-12-31 16:48:56 +0900

    get links and store as dataframe **FINAL**

--- main.py
+++ main.py
@@ -1,20 +1,75 @@
 import requests
-from pymongo import MongoClient
+from urllib.parse import urlparse, urljoin
+from bs4 import BeautifulSoup
+import colorama
+import pandas as pd
+#Init the colorma module
+
+colorama.init()
+GREEN = colorama.Fore.GREEN
+GRAY = colorama.Fore.LIGHTBLACK_EX
+RESET = colorama.Fore.RESET
+YELLOW = colorama.Fore.YELLOW
+
+#initialize the set of links
+internal_urls = set()
+external_urls = set()
+link_df = pd.DataFrame()
+def is_valid(url):
+    parsed = urlparse(url)
+    return bool(parsed.netloc) and bool(parsed.scheme)
+
+def get_all_website_links(url):
+    urls = set()
+    domain_name = urlparse(url).netloc
+    soup = BeautifulSoup(requests.get(url).content, 'html.parser')
+
+    for a_tag in soup.findAll('a'):
+        href = a_tag.attrs.get('href')
+        if href == "" or href is None:
+            continue
+        href = urljoin(url,href)
+        parsed_href = urlparse(href)
+        href = parsed_href.scheme + '://' + parsed_href.netloc + parsed_href.path
+        if not is_valid(href):
+            continue
+        if href in internal_urls:
+            continue
+        if domain_name not in href:
+            if href not in external_urls:
+                print(f"{GRAY}[!] External link: {href}{RESET}")
+                external_urls.add(href)
+            continue
+        print(f"{GREEN}[*] Internal link: {href}{RESET}")
+        urls.add(href)
+        internal_urls.add(href)
+    links = pd.Series(list(internal_urls))
+    link_df[domain_name] = links
+    return urls
+#link_df = interlink를 저장한 dataframe
+
+total_urls_visited = 0
+def crawl(url, max_urls=30):
+
+    global total_urls_visited
+    total_urls_visited += 1
+    print(f"{YELLOW}[*] Crawling: {url}{RESET}")
+    links = get_all_website_links(url)
+    for link in links:
+        if total_urls_visited > max_urls:
+            break
+        crawl(link, max_urls = max_urls)
 
 
-# Reference
-# https://velog.io/@jewon119/02.Python-%ED%99%9C%EC%9A%A9-Crawling-MongoDB
-client = MongoClient('localhost', 5000) #Connect to Database
-invention_db = client.dbsparta #Create database 'invention' and put it on object 'invention_db'
-invention_collection = invention_db.invention_collection #Create collection 'invention_collection' and then put it on object 'invention_collection'
+if __name__ == '__main__':
+    with open('crawled_urls.txt', 'r') as fd:
+        urls = fd.read().splitlines()
+        links = pd.DataFrame()
+        for u in urls:
+            crawl(u)
+            print('[+] Total Internal links: ', len(internal_urls))
+            print('[+] Total External links: ', len(external_urls))
+            print('[+] Total URLs: ', len(external_urls) + len(internal_urls))
+        print(link_df)
 
-invention_list = {'name': None, 'description': None , 'view':None}
-invention_collection.insert_many(''' invention information list''' )
-
-#Find specific keyword
-# Reference
-# https://jinnynote.com/learn/web/%EC%8A%A4%ED%8C%8C%EB%A5%B4%ED%83%80-%ED%8C%8C%EC%9D%B4%EC%8D%AC-%ED%81%AC%EB%A1%A4%EB%A7%81-db/
-key = str(input('Enter keyword'))
-same_name = list(invention_db.invention_collection.find({'name':key}))
-same_desp = list(invention_db.invention_collection.find({'description':key}))
 
